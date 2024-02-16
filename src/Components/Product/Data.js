c2629037c4da27126176d0e5639c65e379f8446b
Parusharam <parusharam4888@gmail.com>
2024-02-16 19:51:19 +0530

    Product_list Updated

--- src/Components/Product/Data.js
+++ src/Components/Product/Data.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Data.css'
-const Data = ({title, img}) => {
+const Data = (props) => {
+  const {title, img} = props
   return (
     <>
     <div className='product-cart-container'>
       <div className='product-cart-box'>
-        <img src={img} alt="" className='img' style={{ filter: 'brightness(0) invert(1)' }}  />
+        <img src={img} alt="" className='img'/>
       </div>
       <div className="product-title">
         <p>{title}</p>
--- src/Components/Product/List.js
+++ src/Components/Product/List.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import Data from './Data'
-
+import allBags from '../assects/ProductIcons/Frame 461.png'
+import vanity from '../assects/ProductIcons/Frame 50.png'
+import tote from '../assects/ProductIcons/Frame 49.png'
+import duffle from '../assects/ProductIcons/Frame 13.png'
+import laptop from '../assects/ProductIcons/Frame 49 (1).png'
+import messenger from '../assects/ProductIcons/Frame 49 (2).png'
+import sling from '../assects/ProductIcons/Frame 22.png'
+import hand from '../assects/ProductIcons/Frame 20.png'
+import buck from '../assects/ProductIcons/Frame 46.png'
 import './Data.css'
 
 const List = () => {
 
     const data = [
-        {title : "All Bag", img: `https://cdn.iconscout.com/icon/premium/png-512-thumb/shopping-bags-2224132-1862785.png?f=webp&w=512`},
-        {title : "Vanity Pouch", img: `https://cdn4.iconfinder.com/data/icons/types-of-bags-5/504/bag-vanity-cosmetic-case-beautician-1024.png`},
-        {title : "Tote Bag", img: `https://cdn.iconscout.com/icon/premium/png-512-thumb/tote-bag-1629456-1382977.png?f=webp&w=512`},
-        {title : "Duffle Bag", img: `https://cdn.iconscout.com/icon/premium/png-512-thumb/duffle-bag-9318117-7601177.png?f=webp&w=512`},
-        {title : "Laptop Sleeve", img: `https://cdn.iconscout.com/icon/free/png-512/free-laptop-computer-graph-chart-company-statics-performance-2-12564.png?f=webp&w=512`},
-        {title : "Messenger Bags", img: `https://cdn.iconscout.com/icon/premium/png-512-thumb/messenger-bag-1629533-1383004.png?f=webp&w=512`},
-        {title : "Sling Bags", img: `https://cdn.iconscout.com/icon/premium/png-512-thumb/sling-bag-3341129-2783294.png?f=webp&w=512`},
-        {title : "Hand Bags", img: `https://cdn.iconscout.com/icon/premium/png-512-thumb/hand-bag-1700288-1445196.png?f=webp&w=512`},
+        {title : "All Bags", img: allBags},
+        {title : "Vanity Pouch", img: vanity },
+        {title : "Tote Bag", img: tote},
+         {title : "Duffle Bag", img: duffle},
+         {title : "Laptop Sleeve", img: laptop},
+        {title : "Messenger Bags", img: messenger},
+    {title : "Sling Bags", img:sling },
+         {title : "Hand Bags", img: hand},
+         {title : "Buck Bags", img: buck},
+        
     ]
 
   return (
